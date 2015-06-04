5a231ad357f8de34fec1a9ad5655fcdd50c9b135
ruipfmendes <ruimendes_10@hotmail.com>
2015-06-04 15:54:54 +0100

    ex 3

--- src/projecto/Projecto.java
+++ src/projecto/Projecto.java
@@ -1,9 +1,16 @@
 package projecto;
 
+import java.io.IOException;
+
 public class Projecto {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Simulation simulation = new Simulation(600000, 1, 0.05);
         simulation.start();
+        /*Ex. 3
+        double alpha = 0.05;
+        int groups = 2;
+        Machines machines = new Machines(alpha, groups);*/
+        
     }
 }
